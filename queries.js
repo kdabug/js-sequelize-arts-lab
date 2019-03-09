2e23a5c079576c035b76abc3dff76fa678598ce7
Mikayda Mills <mills.ma12@gmail.com>
2019-03-09 16:35:26 -0500

    through query 8 - finding art before 1950

--- queries.js
+++ queries.js
@@ -26,25 +26,82 @@ const whitneyArt = async () => {
     }
   });
   const whitArt = await whitney.getArtworks();
+  //const returnedWhitArt = whitArt.map(art => art.dataValues);
+  console.log("*****2****", whitArt);
   return whitArt;
 };
 
 // [3] Write a Sequelize query to find "Early Sunday Morning".
+const findSunday = async () => {
+  const sunday = await Artwork.findOne({
+    where: {
+      name: {
+        [Op.like]: "%Sunday%"
+      }
+    }
+  });
+  console.log("*****3****", sunday.title);
+  return sunday;
+};
 
 // [4] For that artwork, use the `getArtist()` method and log out
 // the name of the artist that made it.
+const sundayArtist = async () => {
+  const sunday = await findSunday();
+  const artist = await sunday.getArtist();
+  console.log("*****4****", artist);
+  return artist;
+};
 
 // [5] A new Museum is opening up, "Eric's Museum of Stickers"
+const newMuseum = async () => {
+  const ericMuseum = await Museum.create({
+    name: "Eric's Museum of Stickers"
+  });
+};
 
 // [6] Based on an article about naming conventions, Eric decides
 // to rename the museum. Update the name to "Eric's Museum of Curious Stickers"
 // in the database.
+const changeMuseumName = async () => {
+  const eric = await Museum.findOne({
+    where: {
+      name: {
+        [Op.like]: "%Eric%"
+      }
+    }
+  });
+  const newName = eric.update({
+    name: "Eric's Museum of Curious Stickers"
+  });
+  console.log("*****6****", newName.name);
+};
 
 // [7] Eric's museum, although appreciated by a niche population,
 // doesn't make enough to cover the costs of a Lower East Side
 // storefront. Remove the museum from the database.
+const deleteMuseum = async () => {
+  const eric = await Museum.findOne({
+    where: {
+      name: {
+        [Op.like]: "%Eric%"
+      }
+    }
+  });
+  const deleteEric = await eric.destroy();
+};
 
 // [8] Write a query to find artworks from before 1950.
+const oldArt = async () => {
+  const oldArts = await Artwork.findAll({
+    where: {
+      year: {
+        [Op.lte]: 1950
+      }
+    }
+  });
+  console.log("*****8****", oldArts);
+};
 
 // [9] You decide to take a class in modern art history, and want to
 // go see some relevant artworks.
