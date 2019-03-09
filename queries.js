6f3f7c609f7fca9f6a3740bc4ab4fe24ec431171
Mikayda Mills <mills.ma12@gmail.com>
2019-03-09 15:02:42 -0500

    query 1 - find artwork in whitney

--- queries.js
+++ queries.js
@@ -1,48 +1,59 @@
-const { Museum, Artwork, Artist } = require('./models');
+const { Museum, Artwork, Artist } = require("./models");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
+// [1] Write a Sequelize query to find the Whitney from the database.
+const findWhitney = async () => {
+  const whitney = await Museum.findOne({
+    where: {
+      name: {
+        [Op.like]: "%Whitney%"
+      }
+    }
+  });
+  console.log("*****1****", whitney.name);
+};
 
-  // [1] Write a Sequelize query to find the Whitney from the database.
-  const findWhitney = async () => {
-    const whitney = await Museum.findOne({
-      where: {
-        name: {
-          [Op.like]: '%Whitney%',
-        },
-      },
-    });
-    console.log('*****1****', whitney.name);
-  };
+// [2] Use the `getArtworks()` method and log the name
+// of each artwork at the Whitney.
+const whitneyArt = async () => {
+  const whitney = await Museum.findOne({
+    where: {
+      name: {
+        [Op.like]: "%Whitney%"
+      }
+    }
+  });
+  const whitArt = await whitney.getArtworks();
+  return whitArt;
+};
 
-  // [2] Use the `getArtworks()` method and log the name
-  // of each artwork at the Whitney.
+// [3] Write a Sequelize query to find "Early Sunday Morning".
 
-  // [3] Write a Sequelize query to find "Early Sunday Morning".
+// [4] For that artwork, use the `getArtist()` method and log out
+// the name of the artist that made it.
 
-  // [4] For that artwork, use the `getArtist()` method and log out
-  // the name of the artist that made it.
+// [5] A new Museum is opening up, "Eric's Museum of Stickers"
 
-  // [5] A new Museum is opening up, "Eric's Museum of Stickers"
+// [6] Based on an article about naming conventions, Eric decides
+// to rename the museum. Update the name to "Eric's Museum of Curious Stickers"
+// in the database.
 
-  // [6] Based on an article about naming conventions, Eric decides
-  // to rename the museum. Update the name to "Eric's Museum of Curious Stickers"
-  // in the database.
+// [7] Eric's museum, although appreciated by a niche population,
+// doesn't make enough to cover the costs of a Lower East Side
+// storefront. Remove the museum from the database.
 
-  // [7] Eric's museum, although appreciated by a niche population,
-  // doesn't make enough to cover the costs of a Lower East Side
-  // storefront. Remove the museum from the database.
+// [8] Write a query to find artworks from before 1950.
 
-  // [8] Write a query to find artworks from before 1950.
-
-  // [9] You decide to take a class in modern art history, and want to
-  // go see some relevant artworks.
-  // Write a query to find artworks created after 1940 made out of Oil Paint.
+// [9] You decide to take a class in modern art history, and want to
+// go see some relevant artworks.
+// Write a query to find artworks created after 1940 made out of Oil Paint.
 
 const main = async () => {
   await findWhitney();
 
   await process.exit();
 };
-
-
 
 main();
--- seed.js
+++ seed.js
@@ -1,35 +1,71 @@
-const { Museum, Art, Artist } = require('./models');
+const { Museum, Artist, Artwork } = require("./models");
 
 const main = async () => {
   // [1] Delete everything in the database.
+  await Artwork.destroy({
+    where: {}
+  });
+  await Artist.destroy({
+    where: {}
+  });
+  await Museum.destroy({
+    where: {}
+  });
 
-
-
-  // [2] Using your museum models, create two museums: 
+  // [2] Using your museum models, create two museums:
   // a. The Whitney Museums
   // b. Cooper-Hewitt Smithsonian Design Museum
 
+  const whitney = await Museum.create({
+    name: "The Whitney Museums"
+  });
+  const cooperHewitt = await Museum.create({
+    name: "Cooper-Hewitt Smithsonian Design Museum"
+  });
 
-  
-
-  // [3] Using your artists models, create three artists: 
+  // [3] Using your artists models, create three artists:
   // a. Edward Hopper
   // b. Elizabeth Murray
   // c. Louis Comfort Tiffany
 
+  const hopper = await Artist.create({
+    name: "Edward Hopper"
+  });
+  const murray = await Artist.create({
+    name: "Elizabeth Murray"
+  });
+  const tiffany = await Artist.create({
+    name: "Louis Comfort Tiffany"
+  });
 
-
-
-  // [4] Using your artworks models, create three artworks: 
+  // [4] Using your artworks models, create three artworks:
   // a. Early Sunday Morning (1930, Oil Paint) by Edward Hopper, owned by the Whitney
   // b. Children Meeting (1978, Oil Paint) by Elizabeth Murray, owned by the Whitney
   // c. Peacock Vase (1890, Glass) by Louis Comfort Tiffany, owned by the Cooper-Hewitt
-
-
-
-  
 };
 
+const sunday = await Artwork.create({
+  title: "Early Sunday Morning",
+  year: 1930,
+  medium: "Oil Paint"
+});
+const children = await Artwork.create({
+  title: "Children Meeting",
+  year: 1978,
+  medium: "Oil Paint"
+});
+const vase = await Artwork.create({
+  title: "Peacock Vase",
+  year: 1890,
+  medium: "Glass"
+});
+
+await sunday.setArtist(hopper);
+await sunday.setMuseum(whitney);
+await children.setArtist(murray);
+await children.setMuseum(whitney);
+await vase.setArtist(tiffany);
+await vase.setMuseum(cooperHewitt);
 
 main();
 
