5f8ac8b7e8449f2b96961e3e2803e47d8de46492
Mikayda Mills <mills.ma12@gmail.com>
2019-03-09 14:32:43 -0500

    created models Artist, Museum, Artwork in model.js

--- models.js
+++ models.js
@@ -1,64 +1,64 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
-  database: 'sequelize_art',
-  dialect:  'postgres',
-  define:   {
+  database: "sequelize_art",
+  dialect: "postgres",
+  define: {
     underscored: true,
-    returning:   true,
-  },
+    returning: true
+  }
 });
 
-
 // Create your models here...
-
 
 /**
  * [1] Write a Sequelize model for museums. It should include columns for:
  *  - name
  */
+const Museum = sequelize.define("museum", {
+  name: Sequelize.STRING
+});
 
-
- 
 /**
  * [2] Write a model for artwork. It should include columns for:
  *  - title
  *  - year
  *  - medium
  */
-
-
+const Artwork = sequelize.define("artwork", {
+  title: Sequelize.STRING,
+  year: Sequelize.INTEGER,
+  medium: Sequelize.STRING
+});
 
 /**
  * [3] Write a model for an artist. It should include columns for:
  *  - name
  */
 
-
- 
+const Artist = sequelize.define("artist", {
+  name: Sequelize.STRING
+});
 
 /**
  * [4] Relate museums and artworks with the sequelize methods
  * - `hasMany()`
  * - `belongsTo()`
  */
-
-
- 
-
-
+Museum.hasMany(Artwork, { onDelete: "cascade" });
+Artwork.belongsTo(Museum);
 /**
  * [5] Relate artworks and artists with the same methods.
  */
-
- 
-
-
+Artist.hasMany(Artwork, { onDelete: "cascade" });
+Artwork.belongsTo(Artist);
 
 module.exports = {
-/**
- * [6] Export the models from the file
- */
-
- 
+  /**
+   * [6] Export the models from the file
+   */
+  Museum,
+  Artist,
+  Artwork,
+  sequelize
 };
